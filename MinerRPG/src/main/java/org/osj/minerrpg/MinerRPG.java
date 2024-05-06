1b50e9f551544916a2347ac186f5bdf5210eb0a7
Oseojin <oseojin5137@gmail.com>
2024-05-06 12:51:31 +0900

    java project setting

---
+++ MinerRPG/src/main/java/org/osj/minerrpg/MinerRPG.java
@@ -0,0 +1,17 @@
+package org.osj.minerrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinerRPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
